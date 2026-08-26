76a417926f8992e5c144c575e4a1265459b6c463
raphaelvieirabortolucci <raphaelvieirabortolucci@gmail.com>
2026-08-26 13:54:26 -0300

    first commit

---
+++ lopal/variaveisBD.py
@@ -0,0 +1,5 @@
+numeroOrdemProd = int(input("Digite o número de ordem do produto: "))
+nomePeça = input("Digite o nome da peça: ")
+IndentificadorMaquina = input("Digite o indentificador da maquina: ")
+
+print(f"OP {numeroOrdemProd} {nomePeça} {IndentificadorMaquina}")
