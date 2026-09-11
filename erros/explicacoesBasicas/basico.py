4a7fd74c0e94fd757e407473ac953ff097d96419
Raphael Vieira Bortolucci <25121@etehort.ps.sp>
2026-09-10 22:31:23 -0300

    adição da explicacao do tratamento de erros

---
+++ erros/explicacoesBasicas/basico.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        numero = int(input("Digite um número: "))
+
+    except ValueError:
+        print("Você não digitou um número!")
+
+    else:
+        print("Número aceito:", numero)
+
+    finally:
+        print("Fim da operação.")
+        break
